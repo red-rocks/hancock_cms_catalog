d6962819c2e91fe1608d17da95831b6f50b9338b
Alexander Kiseliev <ack@CaH4aC>
2016-11-09 09:21:54 +0300

    rails_admin method fixes

--- app/models/concerns/hancock/catalog/decorators/item_image.rb
+++ app/models/concerns/hancock/catalog/decorators/item_image.rb
@@ -7,7 +7,7 @@ module Hancock::Catalog::Decorators
       # def self.default_cache_keys
       #   []
       # end
-      # 
+      #
       # # after_save :image_auto_rails_admin_jcrop
       # def image_auto_rails_admin_jcrop
       #   auto_rails_admin_jcrop(:image) # or nil for cancel autocrop
@@ -30,19 +30,19 @@ module Hancock::Catalog::Decorators
       # def self.rails_admin_add_config(config)
       # end
       #
-      # def admin_can_user_defined_actions
+      # def self.admin_can_user_defined_actions
       #   [].freeze
       # end
-      # def admin_cannot_user_defined_actions
+      # def self.admin_cannot_user_defined_actions
       #   [].freeze
       # end
-      # def manager_can_user_defined_actions
+      # def self.manager_can_user_defined_actions
       #   [].freeze
       # end
-      # def manager_cannot_user_defined_actions
+      # def self.manager_cannot_user_defined_actions
       #   [].freeze
       # end
-      # def rails_admin_user_defined_visible_actions
+      # def self.rails_admin_user_defined_visible_actions
       #   [].freeze
       # end
 
