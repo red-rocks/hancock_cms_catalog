7c7b96c87178665c5d93a9d7189a5eda68eedb39
Alexander Kiseliev <i43ack@gmail.com>
2019-07-18 16:32:48 +0300

    routes + i18n

--- lib/hancock/catalog/routes.rb
+++ lib/hancock/catalog/routes.rb
@@ -7,24 +7,20 @@ module ActionDispatch::Routing
         use_catalog_path: true,
         catalog_path: '/',
         catalog_scope: '/catalog',
-        classes: {
-          items: :items,
-          categories: :categories,
-          catalog_controller: :categories
-        },
-        paths: {
-          items: :items,
-          categories: :categories
+        resources: {
+          items: {
+            only: :show
+          },
+          categories: {
+            only: :show
+          },
         },
         pagination: {
           items: true,
           categories: true,
           category_items: true
         },
-        actions: {
-          items: [:show],
-          categories: [:show],
-        }
+        catalog_controller: :categories
       }
       routes_config.deep_merge!(config)
 
@@ -35,14 +31,14 @@ module ActionDispatch::Routing
 
     private
     def generate_hancock_catalog_user_routes(routes_config)
-      if !routes_config[:use_items_path] and !routes_config[:classes][:items].nil?
-        resources routes_config[:classes][:items].to_sym, only: routes_config[:actions][:items], path: routes_config[:paths][:items] do
+      if !routes_config[:use_items_path] and routes_config[:resources][:items] != false
+        resources :items, routes_config[:resources][:items] || {} do
           get '(/page/:page)', action: :index, on: :collection, as: "" if routes_config[:pagination][:items]
         end
       end
 
-      if !routes_config[:use_categories_path] and !routes_config[:classes][:categories].nil?
-        resources routes_config[:classes][:categories].to_sym, only: routes_config[:actions][:categories], path: routes_config[:paths][:categories] do
+      if !routes_config[:use_categories_path] and routes_config[:classes][:categories] != false
+        resources :categories, routes_config[:resources][:categories] || {} do
           get '(/page/:page)', action: :index, on: :collection, as: "" if routes_config[:pagination][:categories]
           get 'items(/page/:page)', action: :items, on: :member, as: :items if routes_config[:pagination][:category_items]
         end
@@ -56,20 +52,20 @@ module ActionDispatch::Routing
     def generate_hancock_catalog_cms_routes(routes_config)
       scope module: 'hancock' do
         scope routes_config[:catalog_scope], module: 'catalog' do
-          if routes_config[:use_items_path] and !routes_config[:classes][:items].nil?
-            resources routes_config[:classes][:items].to_sym, only: routes_config[:actions][:items], path: routes_config[:paths][:items], as: :hancock_catalog_items do
+          if routes_config[:use_items_path] and routes_config[:resources][:items] != false
+            resources :items, routes_config[:resources][:items] || {} do
               get '(/page/:page)', action: :index, on: :collection, as: "" if routes_config[:pagination][:items]
             end
           end
 
-          if routes_config[:use_categories_path] and !routes_config[:classes][:categories].nil?
-            resources routes_config[:classes][:categories].to_sym, only: routes_config[:actions][:categories], path: routes_config[:paths][:categories], as: :hancock_catalog_categories do
+          if routes_config[:use_categories_path] and routes_config[:resources][:categories] != false
+            resources :categories, routes_config[:resources][:categories] || {} do
               get '(/page/:page)', action: :index, on: :collection, as: "" if routes_config[:pagination][:categories]
               get 'items(/page/:page)', action: :items, on: :member, as: :items if routes_config[:pagination][:category_items]
             end
           end
-          if routes_config[:use_catalog_path] and !routes_config[:classes][:catalog_controller].nil?
-            get "#{routes_config[:catalog_path]}" => "#{routes_config[:classes][:catalog_controller]}#index", as: :hancock_catalog
+          if routes_config[:use_catalog_path] and !routes_config[:catalog_controller].nil?
+            get "#{routes_config[:catalog_path]}" => "#{routes_config[:catalog_controller]}#index", as: routes_config[:catalog_path]
           end
         end
       end
